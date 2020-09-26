13091302b35c9c4cd094abda8658e7564fd1aebe
tom <tom@example.com>
2020-09-26 04:18:34 +0000

    put the correct IP address for sa-webapp backend

--- sa-frontend/src/App.js
+++ sa-frontend/src/App.js
@@ -20,7 +20,7 @@ class App extends Component {
     };
 
     analyzeSentence() {
-        fetch('http://localhost:8080/sentiment', {
+        fetch('http://64.225.87.114/sentiment', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
